54aab4ebb2a39799b7e368e9b74a925a99c39eec
Gustav Vingtoft Andersen <44088380+ggustav1@users.noreply.github.com>
2020-02-07 12:44:56 +0100

    Add files via upload

---
+++ NovoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NovoViewController.swift
+//  HelloWorld
+//
+//  Created by Gustav Vingtoft Andersen on 07/02/2020.
+//  Copyright © 2020 Gustav Vingtoft Andersen. All rights reserved.
+//
+
+import UIKit
+
+class NovoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Gustav Vingtoft Andersen on 07/02/2020.
+//  Copyright © 2020 Gustav Vingtoft Andersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
